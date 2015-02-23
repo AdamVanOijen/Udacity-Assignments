864a6a9a93caae3aa5815967a6f84629c6753577
AdamVanOijen <adamvanoijen1997@gmail.com>
2015-02-24 04:38:36 +1300

    Project one has been updated to meet the requirements of the 'Comments' section of the marking criteria, as the previos version did not meet this criteria.

--- Project 1 - movie trailer/Class_Objects/movies.py
+++ Project 1 - movie trailer/Class_Objects/movies.py
@@ -1,8 +1,9 @@
-#creating a class - defines the contents of the objects in the instance of the 'Movie' class.
+"""defining a class with name 'Movie'- defines the contents of the objects in the instance of this class."""
 class Movie ():
-	#on instantiation, the members: 'title', 'poster_image_url',
-	#'trailer_youtube_url' and 'description' will be assigned the values of: 'name', 'URL' and 'Link' and 'summary'
+	"""Instances of this class contain information on a given movie"""
+
 	def __init__(self, name, URL, Link, summary): 
+		"""assigns the values of the class' arguments to their corresponding members on instantiation"""
 		self.title = name
 		self.poster_image_url = URL
 		self.trailer_youtube_url = Link
