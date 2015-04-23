a5618c9e5e694d9dbf2f40f0218f520e8b08093f
AdamVanOijen <adamvanoijen1997@gmail.com>
2015-04-24 07:01:15 +1200

    update

--- Project 2 -tournament/tournament.py
+++ Project 2 -tournament/tournament.py
@@ -7,69 +7,6 @@ def connect():
 
 PG = connect()
 DB = PG.cursor()
-
-#Creating a view that contains records of each player's wins and matches -- player standings
-DB.execute('''CREATE VIEW player_standings AS 
-    SELECT 
-        players.player_ID, 
-        players.name, 
-        CASE WHEN win_count.wins IS Null THEN 0 ELSE win_count.wins END, 
-        --store 0 wins as '0' rather than 'null'
-        CASE WHEN match_count.matches IS Null THEN 0 ELSE match_count.matches END, 
-        --store 0 matches as '0' rather than 'null'
-        row_number() over(PARTITION BY wins ORDER BY win_count.wins DESC) AS nrow 
-        --numbering each row of each partition; implemented to be used by 'pairs' view
-    FROM players                                                                    
-    LEFT JOIN                                                                      
-        (
-        SELECT 
-            player_ID, 
-            count(*) AS matches 
-            --counts how many times each player_ID appears across the rows of a table, to give the match count
-        FROM 
-            players, 
-            matches 
-        WHERE 
-            (players.player_ID = matches.player1 OR players.player_ID = player2) 
-        GROUP BY player_ID) 
-    AS match_count 
-    ON match_count.player_ID = players.player_ID 
-    LEFT JOIN 
-        (SELECT --counts how many times a corresponding 'wins' appears for each player over a table
-            player_ID, 
-            count(*) AS wins 
-        FROM 
-            players, matches 
-        WHERE 
-            (matches.player1_result = 'win' AND players.player_ID = matches.player1) 
-            OR (matches.player2_result = 'win' AND players.player_ID = matches.player2) 
-        GROUP BY player_ID) 
-    AS win_count 
-    ON win_count.player_ID = players.player_ID 
-    ORDER BY players.player_ID DESC''')
-
-#Creating a view that contains pairs of players for the next round.
-
-#The query accomodates for situations where there are an odd number of players with the
-#same amount of wins.
-DB.execute('''CREATE VIEW pairs AS 
-    SELECT 
-        player_standings.player_ID AS p1ID, 
-        player_standings.name AS p1Name, 
-        matched_player.player_ID AS p2ID, 
-        matched_player.name as p2Name 
-    FROM player_standings 
-    LEFT JOIN 
-        (SELECT 
-            player_ID, 
-            name, 
-            wins, 
-            nrow 
-        FROM player_standings) 
-    AS matched_player 
-    ON matched_player.wins = player_standings.wins AND matched_player.nrow = player_standings.nrow+1
-    WHERE player_standings.nrow % 2 = 1''')
-
 def deleteMatches():
     """Remove all the match records from the database."""
     DB.execute('DELETE FROM matches')
@@ -106,11 +43,10 @@ def playerStandings():
     """
     DB.execute('select player_ID, name, wins, matches from player_standings order by wins desc')
     standings = DB.fetchall()
-    print standings
     return standings
 
 
-def reportMatch(player1, player2, player1_result, player2_result):
+def reportMatch(player1, player2, player1_result, player2_result):#arguments must be changed in tournament_test module
     """Records the outcome of a single match between two players.
     outcome be winner/loser or winner/winner
     Args:
@@ -129,22 +65,18 @@ def reportMatch(player1, player2, player1_result, player2_result):
     return a
 
 def swissPairings():
-    """Returns a list of pairs of players for the next round of a match."""
+    """Returns a list of pairs of players for the next round of a match."""    
 
     """ If a player cannot be paired with another player, he/she is assigned
     a bye round and recieves a win. 
     If the player has already recieved a bye round, they do not recieve another """
-
-
-
     DB.execute('''SELECT p1ID 
         FROM pairs, matches 
         WHERE p2ID IS NULL AND p1ID != matches.player1 AND p1ID != matches.player2 ''')
         #don't give unmatched player a default win if he has already been allocated one
-    unmatched = DB.fetchall()
+    defaultWinners = DB.fetchall()
     
-    print unmatched
-    for player in unmatched:
+    for player in defaultWinners:
         reportMatch(player, player, 'win', 'win') 
 
     DB.execute("SELECT * FROM pairs")
--- Project 2 -tournament/tournament.sql
+++ Project 2 -tournament/tournament.sql
@@ -10,5 +10,72 @@
 --players table contains general information on each player
 create table players ( name text, player_ID serial primary key );
 --matches table contains information about a match between two players
-create table matches ( player1 serial references players, player2 serial references players, player1_result text, player2_result text, tournament_ID int , match_ID serial, primary key(match_id ) );
+create table matches ( player1 serial references players, player2 serial references players, player1_result text, player2_result text, match_ID serial, primary key(match_id ) );
+
+
+
+
+--Creating a view that contains records of each player's wins and matches -- player standings
+CREATE VIEW player_standings AS 
+    SELECT 
+        players.player_ID, 
+        players.name, 
+        CASE WHEN win_count.wins IS Null THEN 0 ELSE win_count.wins END, 
+        --store 0 wins as '0' rather than 'null'
+        CASE WHEN match_count.matches IS Null THEN 0 ELSE match_count.matches END, 
+        --store 0 matches as '0' rather than 'null'
+        row_number() over(PARTITION BY wins ORDER BY win_count.wins DESC) AS nrow 
+        --numbering each row of each partition; implemented to be used by 'pairs' view
+    FROM players                                                                    
+    LEFT JOIN                                                                      
+        (
+        SELECT 
+            player_ID, 
+            count(*) AS matches 
+            --counts how many times each player_ID appears across the rows of a table, to give the match count
+        FROM 
+            players, 
+            matches 
+        WHERE 
+            (players.player_ID = matches.player1 OR players.player_ID = player2) 
+        GROUP BY player_ID) 
+    AS match_count 
+    ON match_count.player_ID = players.player_ID 
+    LEFT JOIN 
+        (SELECT --counts how many times a corresponding 'wins' appears for each player over a table
+            player_ID, 
+            count(*) AS wins 
+        FROM 
+            players, matches 
+        WHERE 
+            (matches.player1_result = 'win' AND players.player_ID = matches.player1) 
+            OR (matches.player2_result = 'win' AND players.player_ID = matches.player2) 
+        GROUP BY player_ID) 
+    AS win_count 
+    ON win_count.player_ID = players.player_ID 
+    ORDER BY players.player_ID DESC;
+
+
+--Creating a view that contains pairs of players for the next round.
+
+--The query accomodates for situations where there are an odd number of players with the
+--same amount of wins.
+CREATE VIEW pairs AS 
+    SELECT 
+        player_standings.player_ID AS p1ID, 
+        player_standings.name AS p1Name, 
+        matched_player.player_ID AS p2ID, 
+        matched_player.name as p2Name 
+    FROM player_standings 
+    LEFT JOIN 
+        (SELECT 
+            player_ID, 
+            name, 
+            wins, 
+            nrow 
+        FROM player_standings) 
+    AS matched_player 
+    ON matched_player.wins = player_standings.wins AND matched_player.nrow = player_standings.nrow+1
+    WHERE player_standings.nrow % 2 = 1;
+--to accomodate for the extra credit task - I have adapted my table to accomodate for a tie. Therefore, both players are specified, and the outcome for each player is specified
 
